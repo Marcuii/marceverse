6680abcd3b6a0ee19a7eb3df3babdcd2915491d5
Marcelino Ibrahim <marcelino.saad@proton.me>
2025-01-07 22:42:33 +0200

    Resume Added

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-let mybutton = document.querySelector("#myBtn");
 let myNav = document.querySelector(".navbar");
 let myBrand = document.querySelector(".navbar-brand");
+let myNavBtn = document.querySelector(".nav-btn");
 let myCollapse = document.querySelector(".navbar-collapse");
+let myNavLinks = document.querySelector(".navbar-nav");
+let mybutton = document.querySelector("#myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    mybutton.style.display = "block";
-
-    myNav.style.backgroundColor = "rgba(241, 241, 241, 1)";
-    myNav.style.transform = "scaleY(0.8)";
-
-    myBrand.classList.remove('d-none');
+  if (document.body.scrollTop > 3 || document.documentElement.scrollTop > 3) {
+    myNav.style.backgroundColor = "rgba(241, 241, 241, 0.8)";
     
-    myCollapse.classList.replace("col-md-12", "col-md-6");
+    myBrand.classList.replace('d-none', 'd-flex');    
+    
+    myNavBtn.classList.remove("col-12");
+    
+    myCollapse.classList.replace("col-lg-12", "col-lg-6");
     myCollapse.classList.replace("justify-content-center", "justify-content-end");
 
-} else if (document.body.scrollTop < 3 || document.documentElement.scrollTop < 3){
-    mybutton.style.display = "none";
-
+    myNavLinks.classList.remove("nav-text");
+    
+    mybutton.style.display = "block";
+  } else if (document.body.scrollTop < 3 || document.documentElement.scrollTop < 3){
     myNav.style.backgroundColor = "rgba(0, 0, 0, 0)";
-    myNav.style.transform = "scaleY(1)";
-
-    myBrand.classList.add('d-none');
-
-    myCollapse.classList.replace("col-md-6", "col-md-12");
+    
+    myBrand.classList.replace('d-flex', 'd-none');
+    
+    myNavBtn.classList.add("col-12");
+    
+    myCollapse.classList.replace("col-lg-6", "col-lg-12");
     myCollapse.classList.replace("justify-content-end", "justify-content-center")
+    
+    myNavLinks.classList.add("nav-text");
 
-}
-}
+    mybutton.style.display = "none";
+}}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
