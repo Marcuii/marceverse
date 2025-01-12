01df23b63f9f8a4e55a562d8fcf2a87227d0c92e
Marcelino Ibrahim <marcelino.saad@proton.me>
2025-01-13 01:44:20 +0200

    Portfolio Added

--- index.js
+++ index.js
@@ -10,26 +10,28 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 3 || document.documentElement.scrollTop > 3) {
-    myNav.style.backgroundColor = "rgba(241, 241, 241, 0.8)";
+    myNav.style.backgroundColor = "rgba(var(--background-color), 0.8)";
     
-    myBrand.classList.replace('d-none', 'd-flex');    
+    myBrand.classList.replace('d-none', 'd-flex');  
+    myBrand.classList.add('focus-in-contract');
     
     myNavBtn.classList.remove("col-12");
     
-    myCollapse.classList.replace("col-lg-12", "col-lg-6");
+    myCollapse.classList.replace("col-md-12", "col-md-6");
     myCollapse.classList.replace("justify-content-center", "justify-content-end");
 
     myNavLinks.classList.remove("nav-text");
     
     mybutton.style.display = "block";
   } else if (document.body.scrollTop < 3 || document.documentElement.scrollTop < 3){
-    myNav.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    myNav.style.backgroundColor = "rgba(var(--background-color), 0)";
     
     myBrand.classList.replace('d-flex', 'd-none');
+    myBrand.classList.remove('focus-in-contract');
     
     myNavBtn.classList.add("col-12");
     
-    myCollapse.classList.replace("col-lg-6", "col-lg-12");
+    myCollapse.classList.replace("col-md-6", "col-md-12");
     myCollapse.classList.replace("justify-content-end", "justify-content-center")
     
     myNavLinks.classList.add("nav-text");
